fbcd484719cad8cbe2e5e0eb7bf02a058efad876
bobcatalyst <51380671+bobcatalyst@users.noreply.github.com>
2025-02-01 22:25:40 -0500

    fix commandCollect

--- command.go
+++ command.go
@@ -3,77 +3,77 @@ package subflow
 import "fmt"
 
 type Command interface {
-    Command() string
+	Command() string
 }
 
 type CommandArgs interface {
-    Command
-    Args() []string
+	Command
+	Args() []string
 }
 
 type CommandEnv interface {
-    Command
-    Environment() []string
+	Command
+	Environment() []string
 }
 
 type CommandArgsEnv interface {
-    CommandArgs
-    Environment() []string
+	CommandArgs
+	Environment() []string
 }
 
 type basicCommandArgs struct {
-    command string
-    args    []string
-    env     []string
+	command string
+	args    []string
+	env     []string
 }
 
 func NewCommand(command string) Command {
-    return &basicCommandArgs{
-        command: command,
-    }
+	return &basicCommandArgs{
+		command: command,
+	}
 }
 
 func NewCommandArgs(command string, args []string) CommandArgs {
-    return &basicCommandArgs{
-        command: command,
-        args:    args,
-    }
+	return &basicCommandArgs{
+		command: command,
+		args:    args,
+	}
 }
 
 func NewCommandEnv(command string, env []string) CommandEnv {
-    return &basicCommandArgs{
-        command: command,
-        env:     env,
-    }
+	return &basicCommandArgs{
+		command: command,
+		env:     env,
+	}
 }
 
 func NewCommandArgsEnv(command string, args, env []string) CommandArgsEnv {
-    return &basicCommandArgs{
-        command: command,
-        args:    args,
-        env:     env,
-    }
+	return &basicCommandArgs{
+		command: command,
+		args:    args,
+		env:     env,
+	}
 }
 
 // WithEnv appends new environment variables to the command.
 func WithEnv(cmd Command, env []string) CommandEnv {
-    command, args, subEnv := commandCollect(cmd)
-    return &basicCommandArgs{
-        command: command,
-        args:    args,
-        env:     append(subEnv, env...),
-    }
+	command, args, subEnv := commandCollect(cmd)
+	return &basicCommandArgs{
+		command: command,
+		args:    args,
+		env:     append(subEnv, env...),
+	}
 }
 
 func commandCollect(cmd Command) (command string, args, env []string) {
-    command = cmd.Command()
-    if cmd, ok := cmd.(CommandArgs); ok {
-        args = cmd.Args()
-    }
-    if cmd, ok := cmd.(CommandEnv); ok {
-        args = cmd.Environment()
-    }
-    return
+	command = cmd.Command()
+	if cmd, ok := cmd.(CommandArgs); ok {
+		args = cmd.Args()
+	}
+	if cmd, ok := cmd.(CommandEnv); ok {
+		env = cmd.Environment()
+	}
+	return
 }
 
 func (cmd *basicCommandArgs) Command() string       { return cmd.command }
@@ -84,5 +84,5 @@ func (cmd *basicCommandArgs) Environment() []string { return cmd.env }
 type ErrExitCode int
 
 func (err ErrExitCode) Error() string {
-    return fmt.Sprintf("exit code(%d)", err)
+	return fmt.Sprintf("exit code(%d)", err)
 }
